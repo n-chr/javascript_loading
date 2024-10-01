0bd24f6c1978597b436177addc4c2ddaa377d916
n-chr <131326994+n-chr@users.noreply.github.com>
2024-10-01 10:01:05 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+//画面が読み込まれたら animation を呼び出す
+window.addEventListener('load', animation);
+
+// 定数の定義
+const loading = document.getElementById('loading');
+
+// cssクラスを追加する関数
+function animation(){
+  loading.classList.add('loaded');
+}
+
+//画面が読み込まれたら animation を呼び出す
+// window.addEventListener('load', animation);
+
+// 指定秒後に動作させる
+window.setTimeout( animation, 5000 );
